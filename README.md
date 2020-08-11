fe7240784a3d6fbff7eeeab7c394dc474cd29cf1
keffri <keffri.w.neal@gmail.com>
2020-08-11 14:34:43 -0700

    functional grid w/ resize/clear

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Etch A Sketch Project
+
+CSS Grid
--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const gridContainer = document.querySelector(".gridContainer");
+const clear = document.querySelector(".clear");
+let gridCells;
 
-makeCells = () => {
-  for (let i = 0; i < 256; i++) {
-    let gridCell = document.createElement("div");
-    gridCell.classList.add("cellStyle");
-    gridCell.addEventListener("mouseover", (e) =>
-      e.target.classList.add("black")
-    );
-    gridContainer.appendChild(gridCell);
+function makeGrid(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let cells = document.createElement("div");
+    cells.classList.add("gridCell");
+    gridContainer.appendChild(cells);
   }
-};
+  let gridCells = document.querySelectorAll(".gridCell");
+  gridCells.forEach((gridCell) => {
+    gridCell.addEventListener("mouseover", (e) => {
+      gridCell.classList.add("hovered");
+    });
+  });
+}
 
-makeCells();
+function clearGrid() {
+  let size = prompt("Enter the amount of squares you would like on each side.");
+  makeGrid(size);
+  let gridCells = document.querySelectorAll(".gridCell");
+  gridCells.forEach((gridCell) => {
+    gridCell.classList.remove("hovered");
+  });
+}
+
+clear.addEventListener("click", clearGrid);
+
+makeGrid(16);
